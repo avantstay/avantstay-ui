db3c9017e02c5dea235bc9c63e2f25df5b81a80f
Diel Duarte <magdiel.kesller@gmail.com>
2019-10-09 17:27:13 -0300

    fix: only use "use" prefix for hooks (#13)

---
+++ lib/utils/getElementOffset.d.ts
@@ -0,0 +1,8 @@
+export declare function getElementOffset(element: HTMLElement | null, scrollableParent?: HTMLElement): {
+    height: number;
+    width: number;
+    top: number;
+    left: number;
+    bottom: number;
+    right: number;
+};

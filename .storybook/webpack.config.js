ae650b86caef461971f84af04e0ae4d06cbd48c7
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2020-05-14 11:58:50 -0300

    fix: remove CaseSensitivePathsPlugin since it was breaking storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,21 +1,22 @@
-const path = require('path');
-
-module.exports = ({ config, mode }) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader')
-      }
-    ]
-  });
+        loader: require.resolve('awesome-typescript-loader'),
+      },
+    ],
+  })
 
   config.module.rules.push({
     test: /\.stories\.(ts|tsx)?$/,
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre'
-  });
+    enforce: 'pre',
+  })
 
-  config.resolve.extensions.push('.ts', '.tsx');
-  return config;
-};
+  // Remove CaseSensitivePathsPlugin (it causes storybook not to build due to some lodash packages)
+  config.plugins.splice(4, 1)
+
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}

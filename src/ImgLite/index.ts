bb82dba2a832290cd49cb11b9fa163c66b01659b
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2020-05-14 13:17:49 -0300

    fix: update ImgLite exports

--- src/ImgLite/index.ts
+++ src/ImgLite/index.ts
@@ -1,2 +1,1 @@
 export { default } from './ImgLite'
-export * from './ImgLite'
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as DateRangePicker } from './DateRangePicker'
 export { default as FloatingContainer } from './FloatingContainer'
+export { default as ImgLite } from './ImgLite'
 export { default as Tooltip, HorizontalGravity, VerticalGravity } from './Tooltip'

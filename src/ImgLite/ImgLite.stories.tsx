ecdebb1bc94e21a48f757aed75cc322a747e4366
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2020-05-20 15:31:01 -0300

    refactor(ImgLite): use gravity property on stories

--- src/ImgLite/ImgLite.stories.tsx
+++ src/ImgLite/ImgLite.stories.tsx
@@ -8,19 +8,19 @@ const Stories = storiesOf('ImgLite', module)
 
 Stories.add('Default usage', () => <ImgLite src={EXAMPLE_IMAGE} style={{ maxWidth: 500, minHeight: 300, width: '100%' }} />)
 
-Stories.add('Different crops', () => (
+Stories.add('Different gravities', () => (
   <div style={{ alignItems: 'stretch', display: 'flex', height: '100%', justifyContent: 'space-between', minHeight: 500 }}>
     <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', width: '33%' }}>
       <div style={{ marginBottom: '10px' }}>ATTENTION</div>
-      <ImgLite crop="attention" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
+      <ImgLite gravity="attention" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
     </div>
     <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', width: '33%' }}>
       <div style={{ marginBottom: '10px' }}>CENTER</div>
-      <ImgLite crop="center" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
+      <ImgLite gravity="center" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
     </div>
     <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', width: '33%' }}>
       <div style={{ marginBottom: '10px' }}>ENTROPY</div>
-      <ImgLite crop="entropy" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
+      <ImgLite gravity="entropy" src={EXAMPLE_IMAGE} style={{ flexGrow: 1, width: '100%' }} />
     </div>
   </div>
 ))

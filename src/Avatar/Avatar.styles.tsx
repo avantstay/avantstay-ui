6e6a697a4807c522e8823c0ed110ea1ed3e14a11
Wellington Guimaraes <wellguimaraes@gmail.com>
2020-06-17 17:39:59 -0300

    v4.3.0

---
+++ src/Avatar/Avatar.styles.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled'
+
+import { renderIf } from 'react-renderif-hoc'
+
+export const AvatarRoot = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 300ms;
+  flex-grow: 0;
+  flex-shrink: 0;
+  box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.1);
+`
+
+export const Initials = renderIf('show')(styled.span`
+  font-family: ${p => p.theme.fonts.regular};
+  font-weight: bold;
+  color: white;
+  font-size: ${p => p.size * 0.35};
+  line-height: ${p => p.size}px;
+`)

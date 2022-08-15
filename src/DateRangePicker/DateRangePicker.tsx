77637e32e3b9975f1f2ff0f9a03647a113b80f78
Pedro Araujo <paraujo@avantstay.com>
2022-08-15 19:33:16 -0300

    fix: add isBothDates equal prop to fix footer line through

--- src/DateRangePicker/DateRangePicker.tsx
+++ src/DateRangePicker/DateRangePicker.tsx
@@ -62,6 +62,7 @@ export interface DateRangePickerProps {
   originalRange?: DateRange
   totalAmount?: string
   blockedDates?: string[]
+  isBothDatesEqual: boolean
 }
 
 export interface DateRangePickerState {
@@ -93,6 +94,7 @@ class DateRangePicker extends Component<DateRangePickerProps, DateRangePickerSta
     applyLabel: 'Apply',
     horizontalAlignment: 'left',
     singleDateRange: false,
+    isBothDatesEqual: false,
   }
 
   step = 0
@@ -247,6 +249,7 @@ class DateRangePicker extends Component<DateRangePickerProps, DateRangePickerSta
       originalRange,
       totalAmount,
       blockedDates,
+      isBothDatesEqual,
     } = this.props
 
     const { range, link } = this.state
@@ -312,7 +315,7 @@ class DateRangePicker extends Component<DateRangePickerProps, DateRangePickerSta
               <>
                 <Divider />
                 <div>
-                  {range.endDate && !isSameDayRange && (
+                  {!isBothDatesEqual && range.endDate && !isSameDayRange && (
                     <FooterTextLinedThrough>{formatRange(originalRange)}</FooterTextLinedThrough>
                   )}
                   <FooterText>

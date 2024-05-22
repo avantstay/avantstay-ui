a25b61ebfaf9fd00a1766752d59fa55cd0e55cea
Tomasz Przysiwek <tprzysiwek@avantstay.com>
2024-05-22 21:23:41 +0200

    reorder

--- src/DateRangePicker/index.ts
+++ src/DateRangePicker/index.ts
@@ -1,4 +1,4 @@
+import Calendar from './Calendar'
 export { DateRangePicker } from './DateRangePicker'
 export { DateRangePickerBase } from './DateRangePickerBase'
-import Calendar  from './Calendar'
 export { Calendar }
